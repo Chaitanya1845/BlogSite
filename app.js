a89e55e1515aac77768ae5224498ecaf90b9a7e2
Chaitanya Govind Nagdev <nagdev.chaitanya@gmail.com>
2021-10-17 20:05:36 +0530

    BLogSite Secure Testing

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const sessionConfig = {
     saveUninitialized: true,
     cookies: {
         httpOnly: true,
-        // secure: true,
+        secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
